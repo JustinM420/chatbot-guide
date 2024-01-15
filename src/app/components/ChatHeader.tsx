e973426618712521c34882f9a14080c289544c3f
Weedsociety <103718091+Weedsociety@users.noreply.github.com>
2024-01-14 18:58:22 -0800

    first commit

---
+++ src/app/components/ChatHeader.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+
+interface ChatHeaderProps {
+    
+}
+ 
+const ChatHeader: FC<ChatHeaderProps> = ({}) => {
+    return ( 
+        <div className='w-full flex gap-3 justify-start items-center text-zinc-300'>
+            <div className='flex flex-col items-start text-sm'>
+                <p className='text-xs'>
+                    Chat With
+                </p>
+                <div className='flex gap-1.5 items-center'> 
+                    <p className='w-2 h-2 rounded-full bg-green-500' />
+                    <p className='text-md'>Hacking Hub Harry</p>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default ChatHeader;
